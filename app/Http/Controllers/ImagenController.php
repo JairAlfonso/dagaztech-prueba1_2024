85be49574a08ba7e102f35153bd1ee778381b878
JairAlfonso <jairalfonsolinares@gmail.com>
2024-02-21 23:46:51 -0500

    ultima actualizacion

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -15,11 +15,11 @@ class ImagenController extends Controller
         // Obtener el usuario autenticado
         $user = Auth::user();
 
-        // Obtener todas las imágenes cargadas por el usuario autenticado
-        $imagenes = $user->imagenes()->orderBy('id', 'desc')->get();
+        // // Obtener todas las imágenes cargadas por el usuario autenticado
+        // $imagenes = $user->imagenes()->orderBy('id', 'desc')->get();
 
-        // Debug: Imprimir el arreglo de imágenes
-        dd($imagenes);
+        // // Debug: Imprimir el arreglo de imágenes
+        // dd($imagenes);
 
         // Pasar las imágenes a la vista
         return view("home.index", compact("imagenes"));
@@ -33,24 +33,23 @@ class ImagenController extends Controller
     {
         $request->validate([
             'nombre' => 'required|max:255',
-            'imagen' => 'required|image|mimes:jpg,png,jpeg|max:1024',
+            'imagen' => 'required|file|mimes:jpg,png,jpeg|max:1024',
         ]);
 
         // Obtenemos el archivo de la petición
-        $imagenFile = $request->file('imagen');
+        if($imagenFile = $request->file('imagen')){
+            $folder = "imagenesguardadass3";
+            $peso = $imagenFile->getSize();
+            $extension = $imagenFile->extension();
+            $ruta = Storage::disk("s3")->put($folder, $imagenFile, 'public');
+            $imagenFile->archivo()->create([
+                'imagen'=> $imagenFile,
+                'extension'=> $extension,
+                'peso'=> $peso,
+                'local'=> false,
 
-        // Subimos la imagen al bucket de AWS S3
-        try {
-            $rutaImagen = Storage::disk('s3')->put($imagenFile, file_get_contents($imagenFile), 'public');
-        } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Error al cargar la imagen: ' . $e->getMessage());
-        }
-
-        // Guardamos la información de la imagen en la base de datos
-        $imagen = new Imagen();
-        $imagen->nombre = $request->nombre;
-        $imagen->imagen = $rutaImagen; // Guardamos la ruta de la imagen en el bucket de S3
-        $imagen->save();
+            ]);
+        };
 
         return redirect()->route('home.index')->with('success', 'Imagen cargada exitosamente.');
     }
